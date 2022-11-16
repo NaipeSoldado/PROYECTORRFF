ab92725705f57eea06bc38a90a5746b558a10dc5
Joaquin <escobar.morales.joaquin@gmail.com>
2022-11-16 17:12:11 -0300

    Avanzando en clases

--- apps/home/models.py
+++ apps/home/models.py
@@ -14,19 +14,20 @@ class Proveedores (models.Model):
     contacto = models.CharField(max_length=200, null=True)
 
 class Convenio (models.Model):
-    anual = "Anual"
-    semestral = "Semestral"
-    trimestral = "Trimestral"
-    Frecuencias = [anual,semestral,trimestral]
+    Frecuencia = (
+        ('Anual', 'Una vez al año'),
+        ('Semestral', 'Dos veces al año'),
+        ('trimestral', 'Cuatro veces al año'),
+    )
     tag = models.CharField(max_length=200)
     idconvenio = models.CharField(max_length=10)
     nombre = models.CharField(max_length=400)
-    proveedor = models.ForeignKey(Proveedores, on_delete=models.DO_NOTHING)
+    proveedor = models.ForeignKey(Proveedores, on_delete=models.SET_NULL, blank=True, null=True)
     resolucion = models.CharField(max_length=10)
     fechaInicio = models.DateField()
     duracion = models.IntegerField(default=24)
     extension = models.IntegerField(default=0)
     fechaTermino = models.DateField()
-    montoTotal = models.IntegerField()
-    frecMP = models.CharField(max_length=20, choices=Frecuencias)
-    valorCuota = models.IntegerField()
+    montoTotal = models.IntegerField(default=0)
+    frecMP = models.CharField(max_length=20, choices=Frecuencia)
+    valorCuota = models.IntegerField(default=0)
--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,8 @@ env = environ.Env(
     DEBUG=(bool, True)
 )
 
+DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).parent.parent
 CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
